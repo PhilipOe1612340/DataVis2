73d4c0b5a56481c5d3eceae296a72a9a5b7659ae
akrstova <krstova.alisa@gmail.com>
2021-01-08 00:33:31 +0100

    MDS implementation

--- js/CustomMDS.js
+++ js/CustomMDS.js
@@ -1,4 +1,4 @@
-class CustomMDS extends CustomHTMLElement{
+class CustomMDS extends CustomHTMLElement {
     constructor() {
         super();
         this.projectedData = [];
@@ -6,18 +6,65 @@ class CustomMDS extends CustomHTMLElement{
 
     runComputation() {
         const data = this.transformData(this.data);
-        let mdsData = mdsjs.convertToMatrix(data, true);
-        // TODO somehow MDS returns less rows/points than in the original data
-        console.log(mdsData.rows());
-        mdsjs.landmarkMDSAsync(mdsData, 2, function(points) {
-            points.rowsIter(row => {
-                console.log(row)
-            })
-        });
+        let matrix = druid.Matrix.from(data);
+        const mds = new druid.MDS(matrix);
+        const output = mds.transform()._data;
+        let result = [];
+        for (let i = 0; i < output.length; i += 2) {
+            const point = [output[i], output[i + 1]];
+            const className = this.data[i / 2]['class'];
+            result.push({'data': point, 'class': className});
+        }
+        this.projectedData = this.convertDataset(result);
+
     }
 
     update() {
         this.runComputation();
+
+        this.rootEl.selectAll("*").remove();
+        this.makeContainer();
+
+        // Add X axis
+        const x = d3
+            .scaleLinear()
+            .domain(d3.extent(this.projectedData, (d) => +d.x))
+            .range([0, this.width - margin.left - margin.right]);
+
+        // Add Y axis
+        const y = d3
+            .scaleLinear()
+            .domain(d3.extent(this.projectedData, (d) => +d.y))
+            .range([this.height, margin.top]);
+
+        this.projectedData.forEach((d) => {
+            d.xCoord = x(d.x);
+            d.yCoord = y(d.y);
+        });
+
+        // Plot x axis
+        this.d3Selection
+            .append("g")
+            .attr("transform", "translate(0," + this.height + ")")
+            .call(d3.axisBottom(x).ticks(5));
+
+        // Plot y axis
+        this.d3Selection.append("g").call(d3.axisLeft(y).ticks(5));
+
+        const dots = this.d3Selection.append("g");
+        dots
+            .selectAll("cicle")
+            .data(this.projectedData)
+            .enter()
+            .append("circle")
+            .attr("cx", (d) => d.xCoord)
+            .attr("cy", (d) => d.yCoord)
+            .attr("r", 4)
+            .style("fill", (d) => this.colors[d.className]);
+    }
+
+    convertDataset(data) {
+        return data.map((elem, i) => new DataNode(i, elem.data[0], elem.data[1], elem.class));
     }
 }
 
